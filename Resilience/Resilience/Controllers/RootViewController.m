bd85135fa6d7a67094cdfede25381f44987e2ead
Andrew Spinks <aspinks@dius.com.au>
2012-12-24 16:38:07 +1100

    Convert main layout to autoLayout.

--- Resilience/Resilience/Controllers/RootViewController.m
+++ Resilience/Resilience/Controllers/RootViewController.m
@@ -10,8 +10,10 @@
 
 @property (nonatomic, strong) UIButton *mapButton;
 @property (nonatomic, strong) UIButton *listButton;
+@property (nonatomic, strong) UIView *tabBar;
 @property (nonatomic, strong) IssueListViewController *issueListViewController;
 @property (nonatomic, strong) IssueMapViewController *issueMapViewController;
+@property (nonatomic, strong) NSArray *containerConstraints;
 
 @end
 
@@ -19,8 +21,8 @@
 @implementation RootViewController
 
 - (void)viewDidLoad {
-  // TODO: Use Autolayout
   UIButton *addIssueButton = [UIButton buttonWithType:UIButtonTypeCustom];
+//  self.view.translatesAutoresizingMaskIntoConstraints = NO;
   addIssueButton.frame = CGRectMake(0, 0, 55, 51);
   UIImage *buttonImage = [UIImage imageNamed:@"AddButton"];
   [addIssueButton setBackgroundImage:buttonImage forState:UIControlStateNormal];
@@ -28,16 +30,12 @@
   self.navigationItem.rightBarButtonItem = item;
   [addIssueButton addTarget:self action:@selector(addIssue) forControlEvents:UIControlEventTouchUpInside];
 
-  UIView *tabBar = [[UIView alloc] initWithFrame:CGRectMake(0, 356, 320, 60)];
-  CAGradientLayer *gradient = [CAGradientLayer layer];
-  gradient.frame = tabBar.bounds;
-  UIColor *colorTop = [UIColor colorWithRed:(239.f/255.f) green:(239.f/255.f) blue:(239.f/255.f) alpha:1];
-  UIColor *colorBot = [UIColor colorWithRed:(162.f/255.f) green:(162.f/255.f) blue:(162.f/255.f) alpha:1];
-  gradient.colors = [NSArray arrayWithObjects:(id)[colorTop CGColor], (id)[colorBot CGColor], nil];
-  gradient.masksToBounds = YES;
-  [[tabBar layer] insertSublayer:gradient atIndex:0];
+  self.tabBar = [[UIView alloc] initWithFrame:CGRectZero];
+  self.tabBar.translatesAutoresizingMaskIntoConstraints = NO;
 
   self.listButton = [UIButton buttonWithType:UIButtonTypeCustom];
+  self.listButton.accessibilityIdentifier = @"listButton";
+  self.listButton.translatesAutoresizingMaskIntoConstraints = NO;
   [self.listButton setTitle:@" List" forState:UIControlStateNormal];
   [self.listButton setTitleColor:[UIColor colorWithRed:(89.f/255.f) green:(89.f/255.f) blue:(89.f/255.f) alpha:1] forState:UIControlStateNormal];
   [self.listButton addTarget:self action:@selector(showListView) forControlEvents:UIControlEventTouchUpInside];
@@ -46,6 +44,8 @@
 //  [self.listButton setImage:[UIImage imageNamed:@"TabBar-IssueListSelected"] forState:UIControlStateSelected];
 
   self.mapButton = [UIButton buttonWithType:UIButtonTypeCustom];
+  self.mapButton.accessibilityIdentifier = @"mapButton";
+  self.mapButton.translatesAutoresizingMaskIntoConstraints = NO;
   [self.mapButton setTitle:@" Map" forState:UIControlStateNormal];
   [self.mapButton setTitleColor:[UIColor defaultTextColor] forState:UIControlStateNormal];
   [self.mapButton addTarget:self action:@selector(showMapView) forControlEvents:UIControlEventTouchUpInside];
@@ -53,23 +53,44 @@
   self.mapButton.backgroundColor = [UIColor clearColor];
 //  [self.mapButton setImage:[UIImage imageNamed:@"TabBar-IssueMapSelected"] forState:UIControlStateSelected];
 
-  self.mapButton.frame = CGRectMake(tabBar.frame.size.width / 2, 0, tabBar.frame.size.width / 2, tabBar.frame.size.height);
-  self.listButton.frame = CGRectMake(0, 0, tabBar.frame.size.width / 2, tabBar.frame.size.height);
-
   self.issueListViewController = [[IssueListViewController alloc] init];
+  self.issueListViewController.view.translatesAutoresizingMaskIntoConstraints = NO;
   self.issueMapViewController = [[IssueMapViewController alloc] init];
+  self.issueMapViewController.view.translatesAutoresizingMaskIntoConstraints = NO;
 
-  [tabBar addSubview:self.mapButton];
-  [tabBar addSubview:self.listButton];
-  [self.view addSubview:tabBar];
+  [self.tabBar addSubview:self.mapButton];
+  [self.tabBar addSubview:self.listButton];
+  [self.view addSubview:self.tabBar];
 
+//  NSDictionary *views = NSDictionaryOfVariableBindings(self.tabBar, self.mapButton, self.listButton); //shitty
+  NSDictionary *views = @{ @"tabBar": self.tabBar, @"mapButton": self.mapButton, @"listButton": self.listButton};
+  [self.view addConstraints:[NSLayoutConstraint
+          constraintsWithVisualFormat:@"|[tabBar]|"
+                              options:NSLayoutFormatAlignAllLeft
+                              metrics:nil
+                                views:views]];
+  [self.tabBar addConstraints:[NSLayoutConstraint
+          constraintsWithVisualFormat:@"|[listButton(==mapButton)]-[mapButton]|"
+                              options:NSLayoutFormatAlignAllBottom
+                              metrics:nil
+                                views:views]];
+  [self.tabBar addConstraints:[NSLayoutConstraint
+          constraintsWithVisualFormat:@"V:|[listButton(==mapButton)]|"
+                              options:NSLayoutFormatAlignAllBottom
+                              metrics:nil
+                                views:views]];
   [self showListView];
   self.view.backgroundColor = [UIColor whiteColor];
 }
 
-- (void)viewWillLayoutSubviews {
-  self.issueListViewController.view.frame = CGRectMake(0, 0, 320, 356);
-  self.issueMapViewController.view.frame = CGRectMake(0, 0, 320, 356);
+- (void)viewDidLayoutSubviews {
+  CAGradientLayer *gradient = [CAGradientLayer layer];
+  gradient.frame = self.tabBar.bounds;
+  UIColor *colorTop = [UIColor colorWithRed:(239.f/255.f) green:(239.f/255.f) blue:(239.f/255.f) alpha:1];
+  UIColor *colorBot = [UIColor colorWithRed:(162.f/255.f) green:(162.f/255.f) blue:(162.f/255.f) alpha:1];
+  gradient.colors = [NSArray arrayWithObjects:(id)[colorTop CGColor], (id)[colorBot CGColor], nil];
+  gradient.masksToBounds = YES;
+  [[self.tabBar layer] insertSublayer:gradient atIndex:0];
 }
 
 - (void) addIssue {
@@ -97,13 +118,30 @@
   [firstController willMoveToParentViewController:nil];
   [firstController removeFromParentViewController];
   [firstController.view removeFromSuperview];
-  secondController.view.alpha = 0.;
-
+  UIView *viewToAdd = secondController.view;
+  viewToAdd.alpha = 0.;
   [self addChildViewController:secondController];
-  [[self view] addSubview:secondController.view];
+  [self.view addSubview:viewToAdd];
+
+  NSDictionary *views = @{ @"viewToAdd": viewToAdd, @"tabBar": self.tabBar};
+  if(self.containerConstraints)
+    [self.view removeConstraints:self.containerConstraints];
+
+  self.containerConstraints = [NSLayoutConstraint
+          constraintsWithVisualFormat:@"V:|[viewToAdd][tabBar(==60)]|"
+                              options:0
+                              metrics:nil
+                                views:views];
+  self.containerConstraints = [self.containerConstraints arrayByAddingObjectsFromArray:[NSLayoutConstraint
+          constraintsWithVisualFormat:@"|[viewToAdd]|"
+                              options:NSLayoutFormatAlignAllLeft
+                              metrics:nil
+                                views:views]];
+  [self.view addConstraints:self.containerConstraints];
+  [self.view setNeedsUpdateConstraints];
 
   [UIView animateWithDuration:.25 delay:0 options:UIViewAnimationOptionAllowAnimatedContent animations:^{
-    secondController.view.alpha = 1.;
+    viewToAdd.alpha = 1.;
   } completion:^(BOOL finished) {
     [secondController didMoveToParentViewController:self];
   }];
