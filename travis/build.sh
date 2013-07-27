24a74a74bb9be7147c4691f85d230a0f9d026bfb
Andrew Spinks <aspinks@dius.com.au>
2013-07-27 12:17:42 +1000

    Set terminal language for travis-ci.

--- travis/build.sh
+++ travis/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+export LC_ALL="en_US.UTF-8"
+
 bundle exec rake debug:build
 bundle exec rake debug:test
--- travis/install_script.sh
+++ travis/install_script.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 set -e
 
+export LC_ALL="en_US.UTF-8"
+
 bundle install
