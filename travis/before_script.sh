aa37a9fb1a75acb867d2cc493005f95680bbb356
Andrew Spinks <aspinks@dius.com.au>
2013-08-31 16:37:18 +1000

    Fix missing directory problem on travis ci.

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -2,4 +2,5 @@
 set -e
 
 export LC_ALL="en_US.UTF-8"
+mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 bundle exec rake setup
--- travis/deploy.sh
+++ travis/deploy.sh
@@ -3,6 +3,4 @@ set -e
 
 export LC_ALL="en_US.UTF-8"
 
-mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-
 bundle exec rake release:deploy
